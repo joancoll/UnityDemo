e04a23417ed3fd2e2bfb32cfc760bb200e3e6c9e
joancoll <joancoll@inspladelestany.cat>
2023-10-20 18:04:53 +0200

    RigidBody Collisions

---
+++ PlayerControllerRBCollisions.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerRBCollisions : MonoBehaviour
+{
+    public float playerSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+    public float enemyBounceForceBack = 5.0f;
+    public float enemyBounceForceUp = 5.0f;
+    public float bounceForceUp = 10.0f;
+    public float bounceForceForward = 10.0f;
+    private Rigidbody rb;
+    private Vector3 playerInput, movement;
+    private float xMovement, zMovement;
+    private bool isGrounded = false; //for collision use
+    private bool playJump = false;
+    private Vector3 collisionDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveControl();
+        JumpControl();
+        LiveControl();
+    }
+
+    void FixedUpdate()
+    {
+        if (playJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playJump = false;
+        }
+    }
+    void MoveControl()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+        zMovement = Input.GetAxis("Vertical");
+        playerInput.Set(xMovement, 0, zMovement);
+        movement = playerSpeed * Time.fixedDeltaTime * playerInput;
+        // Opció 1 - propietat position
+        rb.MovePosition(transform.position + movement);
+        // Opció 2 - mètode Translate()
+        // transform.Translate(movement);
+        // Opció 3 - mètode .AddForce()
+        // rb.AddForce(movement, ForceMode.Impulse);
+    }
+
+    void JumpControl()
+    {
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+        // Input.GetKeyDown(KeyCode.Space)
+        // if (Input.GetButtonDown("Jump") && isGrounded) // collision use
+        {
+            playJump = true; //per executar el salt a FixedUpdate()
+        }
+    }
+
+    void LiveControl()
+    {
+        if (transform.position.y < -10)
+        {
+            Debug.Log("Game Over");
+            // Canvi d'escena
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //Debug.Log("Ground enter");
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Injured!");
+            // Calcula la direcció contrària de la col·lisió
+            // Obté la direcció del xoc
+            collisionDirection = (transform.position - collision.transform.position).normalized;
+            // Aplica una força contrària del sentit del Player 
+            rb.AddForce(collisionDirection * enemyBounceForceBack + Vector3.up * enemyBounceForceUp, ForceMode.Impulse);
+        }
+        if (collision.gameObject.CompareTag("Bounce"))
+        {
+            Debug.Log("Bouncing");
+            // Obté la direcció del xoc en la col·lisió
+            collisionDirection = (collision.transform.position - transform.position).normalized;
+            // Aplica una força de salt en el sentit del Player 
+            rb.AddForce(collisionDirection * bounceForceForward + Vector3.up * bounceForceUp, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //Debug.Log("Ground exit");
+            isGrounded = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("NextLevel"))
+        {
+            Debug.Log("Next Level");
+            // Canvi d'escena
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
