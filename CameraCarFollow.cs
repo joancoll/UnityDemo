abfd9aa1248f50998d2a8cd49a1cfbe1b5b6db35
joancoll <joancoll@inspladelestany.cat>
2023-11-09 12:18:31 +0100

    CameraCarFollor update

--- CameraCarFollow.cs
+++ CameraCarFollow.cs
@@ -10,9 +10,10 @@ public class CameraCarFollow : MonoBehaviour
 	public float followSpeed = 2;
 	[Range(1, 10)]
 	public float lookSpeed = 5;
-	Vector3 initialCameraPosition;
-	Vector3 initialCarPosition;
-	Vector3 absoluteInitCameraPosition;
+	Vector3 initialCarPosition, currentCarPosition;
+	Vector3 initialCameraPosition, absoluteInitCameraPosition, targetPosition, lookDirection;
+	Quaternion currentCameraRotation, lookRotation;
+
 
 	void Start()
 	{
@@ -24,13 +25,15 @@ public class CameraCarFollow : MonoBehaviour
 	void FixedUpdate()
 	{
 		//Look at car
-		Vector3 _lookDirection = (new Vector3(carTransform.position.x, carTransform.position.y, carTransform.position.z)) - transform.position;
-		Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
-		transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
+		currentCarPosition = carTransform.position;
+		currentCameraRotation = transform.rotation;
+		lookDirection = currentCarPosition - transform.position;
+		lookRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+		transform.rotation = Quaternion.Lerp(currentCameraRotation, lookRotation, lookSpeed * Time.deltaTime);
 
 		//Move to car
-		Vector3 _targetPos = absoluteInitCameraPosition + carTransform.transform.position;
-		transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
+		targetPosition = absoluteInitCameraPosition + carTransform.transform.position;
+		transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
 
 	}
 
